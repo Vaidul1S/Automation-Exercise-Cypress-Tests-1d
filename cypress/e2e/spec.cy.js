f06bffbada689970a3806ed75d85e2b39553827e
Vaidul1s <vaidas.ad25@gmail.com>
2025-02-19 10:54:29 +0200

    02.19

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -53,31 +53,6 @@ describe('Automation Exercise', () => {
 
   });
 
-  it('Test Case 2: Login User with correct email and password', () => {
-    cy.createUser();
-    cy.visit('https://automationexercise.com/');
-
-    cy.url().should('eq', 'https://automationexercise.com/');
-    cy.get('body').should('be.visible');
-
-    cy.get('ul.navbar-nav li').contains('Signup / Login').click();
-
-    cy.url().should('eq', 'https://automationexercise.com/login');
-    cy.get('body').should('be.visible');
-    cy.get('.login-form').contains('Login to your account').should('be.visible');
-
-    cy.get('input[data-qa="login-email"]').click().type('vaidulistester@test.com');
-    cy.get('input[data-qa="login-password"]').click().type('Vaidulis123');
-    cy.get('button[data-qa="login-button"]').click();
-
-    cy.contains('Logged in as VaidulisTest').should('be.visible');
-
-    cy.contains('Delete Account').click();
-
-    cy.contains('Account Deleted!').should('be.visible');
-
-  });
-
   it('Test Case 3: Login User with incorrect email and password', () => {
     cy.visit('https://automationexercise.com/');
 
@@ -142,6 +117,31 @@ describe('Automation Exercise', () => {
     cy.get('button[data-qa="signup-button"]').click();
 
     cy.contains('Email Address already exist!').should('be.visible');
+
+  });
+
+  it('Test Case 2: Login User with correct email and password', () => {
+    cy.createUser();
+    cy.visit('https://automationexercise.com/');
+
+    cy.url().should('eq', 'https://automationexercise.com/');
+    cy.get('body').should('be.visible');
+
+    cy.get('ul.navbar-nav li').contains('Signup / Login').click();
+
+    cy.url().should('eq', 'https://automationexercise.com/login');
+    cy.get('body').should('be.visible');
+    cy.get('.login-form').contains('Login to your account').should('be.visible');
+
+    cy.get('input[data-qa="login-email"]').click().type('vaidulistester@test.com');
+    cy.get('input[data-qa="login-password"]').click().type('Vaidulis123');
+    cy.get('button[data-qa="login-button"]').click();
+
+    cy.contains('Logged in as VaidulisTest').should('be.visible');
+
+    cy.contains('Delete Account').click();
+
+    cy.contains('Account Deleted!').should('be.visible');
 
   });
 
